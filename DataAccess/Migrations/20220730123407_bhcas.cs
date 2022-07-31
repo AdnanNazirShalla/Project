7211ddd69c23eb244e435d74b84147b8f4bcb9ed
shahharis98 <hs6043811@gmail.com>
2022-07-31 23:35:23 +0530

    new changes
    admin panel

---
+++ DataAccess/Migrations/20220730123407_bhcas.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class bhcas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BankTransaction_Users_Id",
+                table: "BankTransaction");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_BankTransaction",
+                table: "BankTransaction");
+
+            migrationBuilder.RenameTable(
+                name: "BankTransaction",
+                newName: "Transactions");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "userId",
+                table: "Transactions",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Transactions",
+                table: "Transactions",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_userId",
+                table: "Transactions",
+                column: "userId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Users_userId",
+                table: "Transactions",
+                column: "userId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Users_userId",
+                table: "Transactions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Transactions",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_userId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "userId",
+                table: "Transactions");
+
+            migrationBuilder.RenameTable(
+                name: "Transactions",
+                newName: "BankTransaction");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_BankTransaction",
+                table: "BankTransaction",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BankTransaction_Users_Id",
+                table: "BankTransaction",
+                column: "Id",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- IPay/wwwroot/js/admin.js
+++ IPay/wwwroot/js/admin.js
@@ -1,4 +1,5 @@
 ﻿var hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener("click", function () {
     document.querySelector("body").classList.toggle("active");
+    document.querySelector(".ham").classList.toggle("flex");
 });
